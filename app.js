b8d9dedb4278db1745b04b51dd71bae391f8b8a6
John Rasine Irem <rajonirem8@gmail.com>
2022-07-18 14:53:45 +0100

    redact functionality works

--- app.js
+++ app.js
@@ -1,29 +1,82 @@
 let input = document.getElementById("my-text");
 let submitButton = document.getElementById("submit-text");
 let redactedText = document.getElementById("redacted-word");
-submitButton.addEventListener("click", redact);
+
 let replacementText = document.getElementById("replacement-text");
+let answerSpace = document.querySelector(".res");
+let form = document.getElementById("info");
+form.addEventListener("submit", redact);
 let params = {
-  input: "",
-  redacted: "",
-  replacementText: "",
+  input: [],
+  redacted: [],
+  redactedMultiple: [],
+  replacementText: ["***"],
 };
-input.addEventListener("input", (e) => {
-  params.input = e.target.value;
-});
-redactedText.addEventListener("input", (e) => {
-  params.redacted = e.target.value;
-});
-replacementText.addEventListener("input", (e) => {
-  params.replacementText = e.target.value;
-});
+// input.addEventListener("input", (e) => {
+//   params.input.push(e.target.value);
+//   if (params.input.length > 1) {
+//     params.input.shift();
+//   }
+// });
+// redactedText.addEventListener("input", (e) => {
+//   params.redacted.push(e.target.value);
+//   if (params.redacted.length > 1) {
+//     params.redacted.shift();
+//   }
+// });
+// replacementText.addEventListener("input", (e) => {
+//   params.replacementText.push(e.target.value);
+//   if (params.replacementText.length > 1) {
+//     params.replacementText.shift();
+//   }
+// });
 
 function redact(e) {
-  if (params.input !== "" && params.redacted !== "") {
-    let splitWords = params.input.split(" ");
-    if (splitWords.includes(params.redacted)) {
-      let para = document.createElement("p");
-      splitWords.forEach((elem) => {});
+  let paragraph = document.createElement("p");
+  let banned = redactedText.value.split(" ");
+  let changedText = input.value.replaceAll(redactedText.value, "***");
+  let changedTextWithNew = input.value.replaceAll(
+    redactedText.value,
+    replacementText.value
+  );
+  let arr = [];
+  if (replacementText.value === "" && redactedText.value !== "") {
+    if (banned.length < 2) {
+      paragraph.innerText = changedText;
+      console.log(banned);
+    } else {
+      banned.forEach((bannedWord) => {
+        console.log(bannedWord);
+
+        while (changedText.indexOf(bannedWord) !== -1) {
+          changedText = changedText.replace(bannedWord, "***");
+        }
+        paragraph.innerText = changedText;
+      });
+    }
+  } else if (replacementText.value !== "" && redactedText.value !== "") {
+    if (banned.length < 2) {
+      paragraph.innerText = changedTextWithNew;
+      console.log(banned);
+    } else {
+      banned.forEach((bannedWord) => {
+        console.log(bannedWord);
+
+        while (changedTextWithNew.indexOf(bannedWord) !== -1) {
+          changedTextWithNew = changedTextWithNew.replace(
+            bannedWord,
+            replacementText.value
+          );
+        }
+        paragraph.innerText = changedTextWithNew;
+      });
     }
   }
+
+  answerSpace.appendChild(paragraph);
+
+  input.value = "";
+  replacementText.value = "";
+  redactedText.value = "";
+  e.preventDefault();
 }
