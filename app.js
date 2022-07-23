e220e6c7bac0bd93ad3b338d4df96070a5de6a9e
John Rasine Irem <rajonirem8@gmail.com>
2022-07-23 14:57:23 +0100

    fix regex bug

--- app.js
+++ app.js
@@ -1,76 +1,59 @@
 let input = document.getElementById("my-text");
-let submitButton = document.getElementById("submit-text");
 let redactedText = document.getElementById("redacted-word");
 
 let replacementText = document.getElementById("replacement-text");
 let answerSpace = document.querySelector(".res");
 let form = document.getElementById("info");
 form.addEventListener("submit", redact);
-let params = {
-  input: [],
-  redacted: [],
-  redactedMultiple: [],
-  replacementText: ["***"],
-};
-// input.addEventListener("input", (e) => {
-//   params.input.push(e.target.value);
-//   if (params.input.length > 1) {
-//     params.input.shift();
-//   }
-// });
-// redactedText.addEventListener("input", (e) => {
-//   params.redacted.push(e.target.value);
-//   if (params.redacted.length > 1) {
-//     params.redacted.shift();
-//   }
-// });
-// replacementText.addEventListener("input", (e) => {
-//   params.replacementText.push(e.target.value);
-//   if (params.replacementText.length > 1) {
-//     params.replacementText.shift();
-//   }
-// });
 
 function redact(e) {
   let paragraph = document.createElement("p");
+
+  let lcRedacted = new RegExp(redactedText.value, "gi");
   let banned = redactedText.value.split(" ");
-  let changedText = input.value.replaceAll(redactedText.value, "***");
+  let changedText = input.value.replaceAll(lcRedacted, "***");
   let changedTextWithNew = input.value.replaceAll(
-    redactedText.value,
+    lcRedacted,
     replacementText.value
   );
-  let arr = [];
+
   if (replacementText.value === "" && redactedText.value !== "") {
-    if (banned.length < 2) {
+    if (banned.length === 1) {
       paragraph.innerText = changedText;
-      console.log(banned);
+      //console.log(banned);
     } else {
+      console.log(banned);
       banned.forEach((bannedWord) => {
         console.log(bannedWord);
 
-        while (changedText.indexOf(bannedWord) !== -1) {
-          changedText = changedText.replace(bannedWord, "***");
+        while (changedText.toLowerCase().indexOf(bannedWord) !== -1) {
+          let lcBannedWord = new RegExp(bannedWord, "gi");
+          changedText = changedText.replaceAll(lcBannedWord, "***");
+          console.log(changedText);
         }
-        paragraph.innerText = changedText;
       });
+      paragraph.innerText = changedText;
     }
   } else if (replacementText.value !== "" && redactedText.value !== "") {
     if (banned.length < 2) {
       paragraph.innerText = changedTextWithNew;
-      console.log(banned);
+      //console.log(banned);
     } else {
       banned.forEach((bannedWord) => {
-        console.log(bannedWord);
+        //console.log(bannedWord);
 
-        while (changedTextWithNew.indexOf(bannedWord) !== -1) {
-          changedTextWithNew = changedTextWithNew.replace(
-            bannedWord,
+        while (changedTextWithNew.toLowerCase().indexOf(bannedWord) !== -1) {
+          let lcBannedWord = new RegExp(bannedWord, "gi");
+          changedTextWithNew = changedTextWithNew.replaceAll(
+            lcBannedWord,
             replacementText.value
           );
         }
         paragraph.innerText = changedTextWithNew;
       });
     }
+  } else {
+    alert("Please fill in all fields");
   }
 
   answerSpace.appendChild(paragraph);
